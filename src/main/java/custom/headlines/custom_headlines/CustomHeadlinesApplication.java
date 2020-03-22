eb954a5e1f3cf4a4ff87b55d02b498175a2f6b53
Songtaoli001 <1519046057@qq.com>
2020-03-23 00:01:29 +0800

    creat project

---
+++ src/main/java/custom/headlines/custom_headlines/CustomHeadlinesApplication.java
@@ -0,0 +1,13 @@
+package custom.headlines.custom_headlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomHeadlinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomHeadlinesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/custom/headlines/custom_headlines/CustomHeadlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package custom.headlines.custom_headlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomHeadlinesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
